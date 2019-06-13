c33bb3369255dd2e435ebd5dcee26b72a3e8a3a4
Michiel Vanthoor <michielvanthoor@gmail.com>
2019-06-13 21:39:59 +0200

    Removing talisman

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,5 @@
 from flask import Flask
-from flask_talisman import Talisman
 
 app = Flask(__name__)
-talisman = Talisman( app, force_https_permanent='true', force_https='true')
 
 from app import routes
