f5afae3382d9cd87b50585008c41289caccadb53
Michiel Vanthoor <michielvanthoor@gmail.com>
2019-06-13 21:36:12 +0200

    Add Talisman https redirect

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 from flask_talisman import Talisman
 
 app = Flask(__name__)
-Talisman(app)
+talisman = Talisman( app, force_https_permanent='true', force_https='true')
 
 from app import routes
