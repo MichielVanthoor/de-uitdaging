c2ca37d374bc30010f30e8117336b8926aa4ea7c
Michiel Vanthoor <michielvanthoor@gmail.com>
2019-06-13 21:30:37 +0200

    Adding Talisman

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,6 @@
-from flask import render_template, request, redirect
+from flask import render_template
 from app import app
 
-
-@app.before_request
-def before_request():
-    if not request.is_secure:
-        url = request.url.replace("http://", "https://", 1)
-        code = 301
-        return redirect(url, code=code)
 
 @app.route('/')
 @app.route('/index')
