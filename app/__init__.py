de005448e549c49d903021d94bdf4b4a5fceb0ff
Michiel Vanthoor <mvanthoor@mvanthoor-macbookpro3.roam.corp.google.com>
2019-06-14 15:00:19 +0200

    Add new csp rules for talisman

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,12 @@ from flask_talisman import Talisman
 
 app = Flask(__name__)
 csp = {
-    'default-src': '*'
+    'default-src': '\'self\'',
+    'img-src': '*',
+    'media-src': '*',
+    'script-src': '*'
 }
+
 talisman = Talisman(app, content_security_policy=csp)
 
 from app import routes
