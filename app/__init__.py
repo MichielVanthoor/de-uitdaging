e0aaede74bd15fee3ed040cc7e080b507387c71a
Michiel Vanthoor <michielvanthoor@gmail.com>
2019-06-14 23:35:13 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,5 @@
 from flask import Flask
-from flask_talisman import Talisman
 
 app = Flask(__name__)
-csp = {
-    'default-src': '\'self\'',
-    'img-src': '*',
-    'media-src': '*',
-    'script-src': '*'
-}
-
-talisman = Talisman(app, content_security_policy=csp)
 
 from app import routes
