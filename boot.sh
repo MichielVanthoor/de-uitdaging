53a15f4ead66d2fb301706509f49d21c169551dd
Michiel Vanthoor <michielvanthoor@gmail.com>
2019-07-18 09:50:20 +0200

    Make cloudbuild Cloud Run ready

--- boot.sh
+++ boot.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo Starting the engines!
-exec gunicorn -b 0.0.0.0:5000 --access-logfile - --error-logfile - de-uitdaging:app
+exec gunicorn -b 0.0.0.0:$PORT --access-logfile - --error-logfile - de-uitdaging:app
