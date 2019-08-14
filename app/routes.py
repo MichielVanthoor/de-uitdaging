7fdd045a99840ed906faf390e680ea16a992dc73
Michiel Vanthoor <michielvanthoor@gmail.com>
2019-08-14 21:26:16 +0200

    Adding https redirect

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,14 @@
-from flask import render_template
+import firebase_admin
+
 from app import app
+
+from flask import render_template, request, redirect
+from flask_sslify import SSLify
+from werkzeug.contrib.fixers import ProxyFix
+
+default_app = firebase_admin.initialize_app()
+app.wsgi_app = ProxyFix(app.wsgi_app)
+sslify = SSLify(app)
 
 @app.route('/')
 @app.route('/index')
