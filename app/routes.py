87ccb88b2e68c47769a644420fdbb998bf85d302
Michiel Vanthoor <michielvanthoor@gmail.com>
2019-08-16 10:09:00 +0200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -3,15 +3,12 @@ import firebase_admin
 from app import app
 
 from flask import render_template, request
-from flask_talisman import Talisman
+from flask_sslify import SSLify
 from werkzeug.contrib.fixers import ProxyFix
 
 default_app = firebase_admin.initialize_app()
 app.wsgi_app = ProxyFix(app.wsgi_app)
-csp = {
-    'default-src': '\'self\''
-}
-talisman = Talisman(app, content_security_policy=csp)
+SSLify(app)
 
 @app.route('/')
 @app.route('/index')
